f0295fc37bf0b5c3a2b8706c87f091c9153d5852
Jose Kalladanthyil <josekalladanthyil@hotmail.com>
2016-03-12 03:59:27 +0000

    got basic trolley stuff

---
+++ app/src/main/java/Repository/Trolley.java
@@ -0,0 +1,34 @@
+package Repository;
+
+import android.util.Log;
+
+import java.util.List;
+
+import Model.TrolleyItem;
+
+/**
+ * Created by josekalladanthyil on 12/03/16.
+ */
+public class Trolley {
+    private List<TrolleyItem> items;
+
+    public Trolley(List<TrolleyItem> items) {
+        this.items = items;
+    }
+    public List<TrolleyItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<TrolleyItem> items) {
+        this.items = items;
+    }
+
+    public void add(TrolleyItem item) {
+        items.add(item);
+        Log.d("Trolley", "Added item" +item.getName());
+    }
+
+    public void remove(TrolleyItem item) {
+        items.remove(item);
+    }
+}
--- app/src/main/java/realexhack/realexhack/MainActivity.java
+++ app/src/main/java/realexhack/realexhack/MainActivity.java
@@ -2,11 +2,26 @@ package realexhack.realexhack;
 
 import android.os.Bundle;
 
-public class MainActivity extends BaseActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import Model.TrolleyItem;
+import Repository.Trolley;
+
+public class MainActivity extends BaseActivity {
+    public static Trolley trolley;
+    public static Map<String, TrolleyItem> stock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        trolley = new Trolley(new ArrayList<TrolleyItem>());
+        stock = new HashMap<String, TrolleyItem>();
+        populateStocks();
         setContentView(R.layout.activity_main);
+    }
+
+    private void populateStocks() {
+        stock.put("5449000028921", new TrolleyItem(1, "coke", 2,5));
     }
 }
